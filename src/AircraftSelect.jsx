9e43923d0b6361e2ad57343123616987535d2915
blargenfaarg <gerardo.cano338@myci.csuci.edu>
2025-07-06 21:20:43 -0700

    Reformatted

--- src/AircraftSelect.jsx
+++ src/AircraftSelect.jsx
@@ -1,46 +1,46 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import aircraftData from './data/aircraft.json'
 import './AircraftSelect.css';
 
-function AircraftSelect({ label, onSelectAircraft}) {
-    const [selectedAircraftId, setSelectedAircraftId] = useState('');
-    const [aircraftList, setAircraftList] = useState([]);
-  
-    useEffect(() => {
-      const sortedAircraftList = [...aircraftData].sort((a, b) => {
-        return a.name.localeCompare(b.name);
-      });
-      setAircraftList(sortedAircraftList);
-    }, []); 
-  
-    const handleSelectChange = (event) => {
-      const selectedId = event.target.value;
-      setSelectedAircraftId(selectedId);
-  
-      const selectedAircraft = aircraftList.find(aircraft => aircraft.id === selectedId);
-  
-      if (onSelectAircraft) {
-        onSelectAircraft(selectedAircraft);
-      }
-    };
-  
-    return (
-      <div className="aircraft-select-container">
-        <label>{label}</label>
-        <select
-          value={selectedAircraftId}
-          onChange={handleSelectChange}
-          className="aircraft-dropdown"
-        >
-          <option>Select an Aircraft</option>
-          {aircraftList.map((aircraft) => (
-            <option key={aircraft.id} value={aircraft.id}>
-              {aircraft.name}
-            </option>
-          ))}
-        </select>
-      </div>
-    );
-  }
-  
-  export default AircraftSelect;
+function AircraftSelect({ label, onSelectAircraft }) {
+  const [selectedAircraftId, setSelectedAircraftId] = useState('');
+  const [aircraftList, setAircraftList] = useState([]);
+
+  useEffect(() => {
+    const sortedAircraftList = [...aircraftData].sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+    setAircraftList(sortedAircraftList);
+  }, []);
+
+  const handleSelectChange = (event) => {
+    const selectedId = event.target.value;
+    setSelectedAircraftId(selectedId);
+
+    const selectedAircraft = aircraftList.find(aircraft => aircraft.id === selectedId);
+
+    if (onSelectAircraft) {
+      onSelectAircraft(selectedAircraft);
+    }
+  };
+
+  return (
+    <div className="aircraft-select-container">
+      <label>{label}</label>
+      <select
+        value={selectedAircraftId}
+        onChange={handleSelectChange}
+        className="aircraft-dropdown"
+      >
+        <option>Select an Aircraft</option>
+        {aircraftList.map((aircraft) => (
+          <option key={aircraft.id} value={aircraft.id}>
+            {aircraft.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default AircraftSelect;
